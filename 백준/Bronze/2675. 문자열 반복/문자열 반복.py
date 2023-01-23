3928b0f48cf3389299506dce533d49e03254dc46
이병현 <111191932+leebyeonghyeon1@users.noreply.github.com>
2023-01-23 21:18:03 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,12 +1,11 @@
-#T:테스트 케이스의 개수(1<=T<=1000)
-#R:반복횟수(1<=R<=8)
-#S:문자열(S>1)
 T=int(input()) #T를 받는다.
-for i in range(T): #T만큼 반복
+if 1<=T<=1000:pass
+for i in range(T) : #T만큼 반복
     R,S=input().split() #R과 S를 space바 차이로 구분해서 받는다.
     R=int(R) #R은 int형
+    if 1<=R<=8: pass
     S=[R*num for num in S]#S의 각 자리를 R만큼 반복한 list
+    if len(S)>1: pass
     for char in range(len(S)): #S의 자리 수 만큼 char이 0부터 증가
         print(S[char], end="")
     print()
- 
