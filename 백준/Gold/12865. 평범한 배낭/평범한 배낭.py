316d7ce2f7e1c1dcb1b0d89762a7030df822307f
leebyeonghyeon <111191932+leebyeonghyeon1@users.noreply.github.com>
2023-09-09 06:45:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 3356 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (k+1)
+
+for weight, value in items:
+    for j in range(k, weight-1, -1):
+        dp[j] = max(dp[j], dp[j - weight] + value)
+
+print(dp[k])
