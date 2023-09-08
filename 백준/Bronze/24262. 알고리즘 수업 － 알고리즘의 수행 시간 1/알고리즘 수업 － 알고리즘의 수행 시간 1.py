f13a39b6a43f18df9e52242dc51c30698782f0be
leebyeonghyeon <111191932+leebyeonghyeon1@users.noreply.github.com>
2023-09-09 06:13:42 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.py
@@ -0,0 +1,15 @@
+def MenOfPassion(n):
+    # 코드1의 수행 횟수
+    execution_count = 1
+
+    # 상수 시간의 다항식 최고차항의 차수는 0
+    polynomial_degree = 0
+
+    return execution_count, polynomial_degree
+
+
+n = int(input())
+
+count, degree = MenOfPassion(n)
+print(count)
+print(degree)
